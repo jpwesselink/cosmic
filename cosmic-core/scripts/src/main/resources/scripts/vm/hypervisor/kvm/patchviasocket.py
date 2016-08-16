8bef5f4e53e851e4e64c983817b44337ac339945
Remi Bergsma <github@remi.nl>
2016-08-16 17:24:00 +0200

    Use Qemu guest agent to pass cmdline info

--- cosmic-core/scripts/src/main/resources/scripts/vm/hypervisor/kvm/patchviasocket.py
+++ cosmic-core/scripts/src/main/resources/scripts/vm/hypervisor/kvm/patchviasocket.py
@@ -1,22 +1,5 @@
-#!/usr/bin/env python
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+#!/usr/bin/python
 
-#
 # This script connects to the system vm socket and writes the
 # authorized_keys and cmdline data to it. The system VM then
 # reads it from /dev/vport0p1 in cloud_early_config
@@ -24,14 +7,53 @@
 
 import argparse
 import os
+import json
+import base64
 import socket
 
 SOCK_FILE = "/var/lib/libvirt/qemu/{name}.agent"
 PUB_KEY_FILE = "/root/.ssh/id_rsa.pub.cloud"
 MESSAGE = "pubkey:{key}\ncmdline:{cmdline}\n"
+CMDLINE_FILE = "/var/cache/cloud/cmdline_incoming"
+
+FILE_OPEN_WRITE = """{"execute":"guest-file-open", "arguments":{"path":"%s","mode":"w+"}}"""
+FILE_WRITE = """{"execute":"guest-file-write", "arguments":{"handle":%s,"buf-b64":"%s"}}"""
+FILE_CLOSE = """{"execute":"guest-file-close", "arguments":{"handle":%s}}"""
 
 
-def send_to_socket(sock_file, key_file, cmdline):
+# This is only needed temporarily until the new systemvm templates won't have an endless-loop for the old patchviasocket thing.
+def write_to_legacy_socket():
+    try:
+        sock_file = SOCK_FILE.format(name=arguments.name)
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.settimeout(5)
+        s.connect(sock_file)
+        s.sendall("cmdline:Compatibility\n")
+        s.close()
+    except IOError as e:
+        print("ERROR: unable to connect to {0} - {1}".format(sock_file, e.strerror))
+        return 1
+
+def EXE(param):
+    cmd = """virsh qemu-agent-command %s '%s' """ % (arguments.name, param)
+    print "Exe command:%s" % cmd
+    stream = os.popen(cmd).read()
+    return None if not stream else json.loads(stream)
+
+
+def write_guest_file(path, content):
+    file_handle = -1
+    try:
+        file_handle = EXE(FILE_OPEN_WRITE % path)["return"]
+        write_count = EXE(FILE_WRITE % (file_handle, content))["return"]["count"]
+    except Exception as ex:
+        print Exception, ":", ex
+        return -1
+    finally:
+        EXE(FILE_CLOSE % file_handle)
+    return write_count
+
+def generate_content(key_file, cmdline):
     if not os.path.exists(key_file):
         print("ERROR: ssh public key not found on host at {0}".format(key_file))
         return 1
@@ -47,21 +69,16 @@ def send_to_socket(sock_file, key_file, cmdline):
     cmdline = cmdline.replace("%", " ")
 
     msg = MESSAGE.format(key=pub_key, cmdline=cmdline)
+    return msg
 
-    if not os.path.exists(sock_file):
-        print("ERROR: {0} socket not found".format(sock_file))
-        return 1
 
+def write_file():
     try:
-        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        s.connect(sock_file)
-        s.sendall(msg)
-        s.close()
-    except IOError as e:
-        print("ERROR: unable to connect to {0} - {1}".format(sock_file, e.strerror))
-        return 1
-
-    return 0    # Success
+        content = base64.standard_b64encode(generate_content(PUB_KEY_FILE, arguments.cmdline))
+        write_count = write_guest_file(CMDLINE_FILE, content)
+        print write_count > 0
+    except Exception as ex:
+        print "Warning: it was not possible to write to the Qemu Guest Agent at this time. Will try again later."
 
 
 if __name__ == "__main__":
@@ -70,7 +87,5 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--cmdline", required=True, help="Command line")
 
     arguments = parser.parse_args()
-
-    socket_file = SOCK_FILE.format(name=arguments.name)
-
-    exit(send_to_socket(socket_file, PUB_KEY_FILE, arguments.cmdline))
+    write_to_legacy_socket()
+    write_file()
